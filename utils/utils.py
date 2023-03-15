9e0bd5363d540a402e2ba0b3d96e8d0625c01929
sunyue-xfel <82163062+sunyue-xfel@users.noreply.github.com>
2023-03-15 13:58:13 +0100

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import json
+import math
+import numpy as np
+import torch
+import torch.optim as optim
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def get_config_from_json(json_file):
+    """
+    Get the config from a json file
+    :param json_file:
+    :return: config(dictionary)
+    """
+    # parse the configurations from the config json file provided
+    with open(json_file, 'r') as config_file:
+        config_dict = json.load(config_file)
+
+    return config_dict
+
+
+
+def adjust_learning_rate(args, optimizer, epoch):
+    lr = args.learning_rate
+    if args.cosine:
+        eta_min = lr * (args.lr_decay_rate ** 3)
+        lr = eta_min + (lr - eta_min) * (
+                1 + math.cos(math.pi * epoch / args.epochs)) / 2
+    else:
+        steps = np.sum(epoch > np.asarray(args.lr_decay_epochs))
+        if steps > 0:
+            lr = lr * (args.lr_decay_rate ** steps)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
+    if args.warm and epoch <= args.warm_epochs:
+        p = (batch_id + (epoch - 1) * total_batches) / \
+            (args.warm_epochs * total_batches)
+        lr = args.warmup_from + p * (args.warmup_to - args.warmup_from)
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+            
+            
+# SplitBatchNorm: simulate multi-gpu behavior of BatchNorm in one gpu by splitting alone the batch dimension
+# implementation adapted from https://github.com/davidcpage/cifar10-fast/blob/master/torch_backend.py
+class SplitBatchNorm(nn.BatchNorm1d):
+    def __init__(self, num_features, num_splits, **kw):
+        super().__init__(num_features, **kw)
+        self.num_splits = num_splits
+        
+    def forward(self, input):
+        N, C, L = input.shape
+        print('input shape:',input.shape)
+        if self.training or not self.track_running_stats:
+            running_mean_split = self.running_mean.repeat(self.num_splits)
+            running_var_split = self.running_var.repeat(self.num_splits)
+            outcome = nn.functional.batch_norm(
+                input.view(-1, C * self.num_splits, L), running_mean_split, running_var_split, 
+                self.weight.repeat(self.num_splits), self.bias.repeat(self.num_splits),
+                True, self.momentum, self.eps).view(N, C, L)
+            self.running_mean.data.copy_(running_mean_split.view(self.num_splits, C).mean(dim=0))
+            self.running_var.data.copy_(running_var_split.view(self.num_splits, C).mean(dim=0))
+            return outcome
+        else:
+            return nn.functional.batch_norm(
+                input, self.running_mean, self.running_var, 
+                self.weight, self.bias, False, self.momentum, self.eps)
